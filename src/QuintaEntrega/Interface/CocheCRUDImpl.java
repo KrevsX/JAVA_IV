67b9f8a25a72062637b8bfb6e8dfdf22a08124d1
KrevsX <elmrochino,yo@gmail.com>
2022-12-20 15:38:29 -0600

    Update

--- src/QuintaEntrega/Interface/CocheCRUDImpl.java
+++ src/QuintaEntrega/Interface/CocheCRUDImpl.java
@@ -1,25 +1,35 @@
 package QuintaEntrega.Interface;
 
 public class CocheCRUDImpl implements CocheCRUD{
+
+    String save = "Guardar...........";
+    String findAll = "Guardar...........";
+    String delete = "Guardar...........";
+
+    @Override
+    public String toString() {
+        return "CocheCRUDImpl{" +
+                "save='" + save + '\'' +
+                ", findAll='" + findAll + '\'' +
+                ", delete='" + delete + '\'' +
+                '}';
+    }
+
     @Override
     public void save() {
 
         System.out.println("SAVE");
-
     }
 
     @Override
     public void findAll() {
 
         System.out.println("FIND ALL");
-
     }
 
     @Override
     public void delete() {
 
         System.out.println("DELETE");
-
-
     }
 }
--- src/QuintaEntrega/Main/Main.java
+++ src/QuintaEntrega/Main/Main.java
@@ -9,8 +9,16 @@ public class Main {
 
         CocheCRUD cocheCRUD = new CocheCRUDImpl();
 
+        // Version 1
+        System.out.println("        // Version 1\n");
+        System.out.println("-----------------------");
         cocheCRUD.save();
         cocheCRUD.findAll();
         cocheCRUD.delete();
+
+        // Version 2
+        System.out.println("        // Version 2\n");
+        System.out.println("-----------------------");
+        System.out.println(cocheCRUD);
     }
 }
